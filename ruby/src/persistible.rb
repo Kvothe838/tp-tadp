91a37905c6c576efaf1900caec6c966003852d06
Ernesto Bossi <bossi.ernestog@gmail.com>
2021-05-22 12:31:55 -0300

    Limpieza de codigo

--- ruby/src/persistible.rb
+++ ruby/src/persistible.rb
@@ -1,63 +1,45 @@
 # frozen_string_literal: true
-require_relative './tabla'
+require_relative './atributos_persistibles'
+require_relative './persistible_class_methods'
 require_relative './tipo_incorrecto_exception'
+require 'pry'
 
 # Mixin necesario para la clase que desee implementar un ORM.
 module Persistible
 
-  attr_accessor :id, :table
+  attr_accessor :id
+
+  def self.included(klass)
+    klass.extend(ClassMethods)
+  end
 
   def save!
-    nuevo_hash = Hash.new
-    return if @@tabla.nil?
-
-    @@tabla.columnas.each do |col|
-      valor = instance_variable_get "@#{col[:named]}"
-      nuevo_hash[col[:named]] ||= valor
-    end
-
-    tabla = TADB::DB.table(self.class.to_s)
-    @id = tabla.insert(nuevo_hash)
+    return if attr_persistibles.nil?
+    @id = table.insert(attr_persistibles.dame_el_hash(self))
   end
 
   def refresh!
-    tabla = TADB::DB.table(self.class.to_s)
-    unless @id.nil?
-      #obtengo la fila con el id
-      una_fila = tabla.entries.find { |fila| fila[:id] == @id }
-      # por cada par key => value en la fila, con instance_variable_set le asigno el value al symbol
-      # (casteado desde string) que es @algo ej @first_name
-      una_fila.each do |key, value|
-        instance_variable_set("@#{key}", value)
-      end
-    else
+    if @id.nil?
       puts "Este objeto no tiene id"
+      return
+    end
+
+    #obtengo la fila con el id
+    una_fila = self.class.find_by_id_from_table(@id)
+    # por cada par key => value en la fila, con instance_variable_set le asigno el value al symbol
+    # (casteado desde string) que es @algo ej @first_name
+    una_fila.each do |key, value|
+      instance_variable_set("@#{key}", value)
     end
   end
 
   def forget!
-    tabla = TADB::DB.table(self.class.to_s)
-    tabla.delete(@id)
+    table.delete(@id)
     @id = nil
   end
 
-  def validar!
-    @@tabla.columnas.each do |atributo|
-      nombre_atributo = atributo[:named]
-      valor_atributo_instancia = instance_variable_get "@#{nombre_atributo}"
-      clase_correcta_atributo = atributo[:tipo]
-
-      unless valor_atributo_instancia.is_a? clase_correcta_atributo
-        mensaje_exception = "El atributo #{nombre_atributo} no contiene valor de clase #{clase_correcta_atributo}"
-        raise TipoIncorrectoException.new mensaje_exception
-      end
-    end
-    nil
-  end
-
+  #TODO: Ver como mover a ClassMethods
   self.class.class_eval do
-    @@tabla = nil
-
     def define_find_by_method(nombre_columna)
       self.class.define_method("find_by_#{nombre_columna}") do |arg|
         all_instances!.filter { |instancia|
@@ -68,33 +50,19 @@ module Persistible
     unless self.class.instance_methods.include?(:find_by_id)
       define_find_by_method('id')
     end
+  end
 
-    def all_instances!
-      instancias = []
-      filas = TADB::DB.table(to_s).entries
-      filas.each do |una_fila|
-        instancia = new
-        instancia.id = una_fila[:id]
-        instancia.refresh!
-        instancias << instancia
-      end
-      instancias
-    end
+  private
 
-    def has_one(tipo, dato)
-      nombre_tabla = to_s
-      tabla_clase = get_tabla(nombre_tabla)
-      unless tabla_clase.repite_columna(dato)
-        tabla_clase.agregar_columna!(Hash[:tipo, tipo].merge(dato))
-        nombre_columna = dato[:named]
-        define_find_by_method(nombre_columna)
-      end
-    end
+  def validar!
+    attr_persistibles.validar!(self)
+  end
 
-    def get_tabla(nombre)
-      @@tabla = Tabla.new(nombre) if @@tabla.nil?
-      @@tabla
-    end
+  def attr_persistibles
+    self.class.attr_persistibles
+  end
+
+  def table
+    self.class.table
   end
 end
-
