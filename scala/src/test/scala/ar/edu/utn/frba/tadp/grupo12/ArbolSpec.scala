dd8f7ff2cc5f1a6bfb780f67840fb9ec04823885
agustinmonti <agustinmonti20@gmail.com>
2021-07-06 12:31:18 -0300

    Un ajuste a un assert que me decia que 0.9999999999999 no era 1)

--- scala/src/test/scala/ar/edu/utn/frba/tadp/grupo12/ArbolSpec.scala
+++ scala/src/test/scala/ar/edu/utn/frba/tadp/grupo12/ArbolSpec.scala
@@ -32,7 +32,7 @@ class ArbolSpec extends AnyFreeSpec{
       val primerDocena: Double => Apuesta = monto => new Apuesta(Tipo(PrimerDocena,DistribucionEquiprobable),monto)
       // Paso 1) Obtengo las hojas del Arbol generado con una de las posibles combinaciones, el (1,100) es un valor inicial, o semilla,
       // similar a lo que se usa en el foldLeft, 1 es la semilla de probabilidad y 100 el monto
-      val resultados = Casino.generar_arbol_de_apuestas(List(primerDocena(50),primerDocena(30)),(1,100)).dame_tus_hojas
+      val resultados = Casino.generar_arbol_de_apuestas(List(primerDocena(50),primerDocena(30),primerDocena(1000)),(1,100)).dame_tus_hojas
       // Paso 2) Filtro los resultados que tienen la forma (Probabilidad, Monto) segun lo que me interese, en este caso me interesa saber cuales de esos resultados terminan con montos menores a 100 o mayores o iguales a 100
       val resultados_con_montos_menores_a_100 = resultados.filter(a=>a._2 < 100)
       val resultados_con_montos_mayores_a_100 = resultados.filter(a=>a._2 >= 100)
@@ -43,7 +43,7 @@ class ArbolSpec extends AnyFreeSpec{
       println(s"Esto significa que la probabilidad de tener un resultado MENOR a 100 es:${resultados_con_montos_menores_a_100.map(a=>a._1).sum}")
       println(s"Esto significa que la probabilidad de tener un resultado MAYOR o IGUAL a 100 es:${probabilidad_de_que_el_monto_sea_mayor_a_100}")
       // Paso 4) Verifico que la suma de las probabilidades de tener un monto menor a 100 y las de tener un monto mayor o igual a 100 sea igual a 1.
-      assert(resultados_con_montos_menores_a_100.map(a=>a._1).sum+probabilidad_de_que_el_monto_sea_mayor_a_100 == 1)
+      assert(scala.math.abs(1-(resultados_con_montos_menores_a_100.map(a=>a._1).sum+probabilidad_de_que_el_monto_sea_mayor_a_100)) < 0.0001)
     }
   }
 }
