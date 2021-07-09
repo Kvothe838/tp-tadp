deccf3e38ffd495c6618afe997d051804364ff1e
tisaempresa <tisaempresa@gmail.com>
2021-07-09 11:32:46 -0300

    Se sube test sobre jugador con criterio

--- scala/src/test/scala/ar/edu/utn/frba/tadp/grupo12/ApuestasSpec.scala
+++ scala/src/test/scala/ar/edu/utn/frba/tadp/grupo12/ApuestasSpec.scala
@@ -95,20 +95,18 @@ class ApuestasSpec extends AnyFreeSpec{
       println(s" planificacion ${planificacion.map(_.tipo.tipoApuesta)} cantidad ${planificacion.length}")
     }
 
-//    "Dos jugadores apuestan roulette" in {
-//      val roulette = new Roulette
-//      val bob_esponja = new Jugador()
-//      val calamardo_tentaculos = new Jugador()
-//      val apuesta_bob_rojo = new Apuesta(bob_esponja, new Rojo, 50)
-//      val apuesta_calamardo_negro = new Apuesta(calamardo_tentaculos, new Negro, 50)
-//
-//      roulette.apostar(List(apuesta_bob_rojo, apuesta_calamardo_negro))
-//      var resultado = roulette.spin()
-//
-//      val rojos : List[Int] = List[Int](1,3,5,7,9,12,14,16,18,19,21,23,25,27,30,32,34,36)
-//      val salioRojo = rojos.contains(resultado)
-//      assert(salioRojo && bob_esponja.dinero == 150 && calamardo_tentaculos.dinero == 50 ||
-//        !salioRojo && bob_esponja.dinero == 50 && calamardo_tentaculos.dinero == 150)
-//    }
+    "Jugador adicto" in {
+      val criterio : (List[(Double,Double)])=>Double = hojas => hojas.length
+      val adicto = Jugador("Juan Adicto",TipoCriterio(criterio),15)
+      val apuestas = List[Apuesta](
+        new Apuesta(Tipo(Cara,DistribucionEquiprobable),10), // Cara 10
+        new Apuesta(Tipo(Numero(0),DistribucionEquiprobable),15), // 0 15
+        new Apuesta(Tipo(Par,DistribucionEquiprobable),15), // 0 15
+        new Apuesta(Tipo(Numero(2),DistribucionEquiprobable),16)) // 0 15
+
+      val planificacion = Casino.planificar(adicto,apuestas)
+      //println(s" planificacion ${planificacion.map(_.tipo.tipoApuesta)} cantidad ${planificacion.length}")
+      assert(planificacion.length == apuestas.length)
+    }
   }
 }
